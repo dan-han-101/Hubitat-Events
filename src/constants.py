ce441fb5be09ebfab538d7b50c6db227255a346c
programmerdays <emaildhan@gmail.com>
2022-04-05 06:43:22 -0400

    Get all devices and events
    Call hubitat maker api to get all devices and then print events from
    those devices.

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,11 @@
 import os
 
-def get_hubitat_token():
+
+def get_hubitat_base_url() -> str:
+    return "http://hubitat.local/apps/api/391/devices"
+
+
+def get_hubitat_token() -> str:
     return os.getenv(
         "HUBITAT_TOKEN", "Dummy token. Set real one with environment variable"
     )
--- src/hubitat_events.py
+++ src/hubitat_events.py
@@ -1,9 +1,43 @@
 #!/usr/local/bin/python3.9
 
+import requests
+from constants import get_hubitat_base_url, get_hubitat_token
 
-def test():
-    print("Do nothing")
+
+def get_all_devices():
+    url = f"{get_hubitat_base_url()}?access_token={get_hubitat_token()}"
+    r = requests.get(url)
+    if r.status_code == 200:
+        print("get_all_devices succeeded")
+        devices = r.json()
+        return devices
+    else:
+        print(f"get_all_devices failed. status = {r.status_code}")
+        return {}
+
+
+def get_events_for_id(device_id: int):
+    url = (
+        f"{get_hubitat_base_url()}/{device_id}/events?"
+        + f"access_token={get_hubitat_token()}"
+    )
+    r = requests.get(url)
+    if r.status_code == 200:
+        print("get_events_for_id succeeded")
+        return r.json()
+    else:
+        print(f"get_events_for_id failed. status = {r.status_code}")
+        return {}
 
 
 if __name__ == "__main__":
-    test()
+    devices = get_all_devices()
+    for d in devices:
+        print(
+            f"Device name={d['name']},"
+            + f" label={d['label']}, "
+            + f"type={d['label']}"
+        )
+        events = get_events_for_id(d["id"])
+        for e in events:
+            print(f"date={e['date']}, value={e['value']} ")
